27a02fa91fae50ee3dd13fec6ec76f9ff05bf75f
PakshP <112006124+PakshP@users.noreply.github.com>
2022-10-04 22:20:39 -0400

    Add files via upload

---
+++ Lab04Question.java
@@ -0,0 +1,18 @@
+//Paksh Patel
+public class Lab04Question {
+
+	public static void main(String[] args) {
+		
+		for(int n = 1;n <= 5;n++) {
+			
+			if(n==3) {
+				
+				System.exit(0);
+			
+			} System.out.println("Hello");
+			
+		} System.out.println("After the loop");
+		
+	}
+
+}
